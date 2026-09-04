c418fc469e80b82d2e369e9ec12b6666a4d7801f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-04 02:06:30 -0400

    set `STARTUPWMCLASS`
    fixes #11

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/localsend.png
 export DESKTOP=/usr/share/applications/localsend.desktop
+export STARTUPWMCLASS=org.localsend.localsend_app
 
 # Deploy dependencies
 quick-sharun /usr/bin/localsend /usr/lib/localsend
